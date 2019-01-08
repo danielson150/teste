7051495db7651002475ef78c8102b50bbb7710f7
Danielson da Silva Alencar <danielson150@gmail.com>
2019-01-08 10:23:53 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // Códigosm pata inicialização dos frameworks
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('.tabs').tabs();
+    $('.tabs-swipe').tabs({
+        swipeable: true,
+    });
+    $('select').material_select();
+});
